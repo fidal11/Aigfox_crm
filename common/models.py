83b0cb8a21e3a8ff63e12015ebfb9a8576b85887
fidal11 <msfidal2001@gmail.com>
2023-12-01 11:37:46 +0530

    last edit

--- common/models.py
+++ common/models.py
@@ -5,6 +5,21 @@ from django.utils import timezone
 
 
 
-
+class ClockInClockOut(models.Model):
+    timestamp = models.DateTimeField()
+    clock_type = models.CharField(max_length=10)  
+    email = models.EmailField()   
+    
+    class Meta:
+        db_table = 'clockinclockout_tb'
+     
+    
+    
+class Break(models.Model):
+    timestamp_start = models.DateTimeField(null=True)
+    timestamp_end = models.DateTimeField(null=True)
+    email = models.EmailField()   
+    class Meta:
+        db_table = 'break_tb'
 
 
